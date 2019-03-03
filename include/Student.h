57483fdbeef15cf66799d79af5288f00fe436bcf
iamilyasedunov <iliasedunov@yandex.ru>
2019-03-03 15:11:28 +0300

    B18 all tasks

--- include/Student.h
+++ include/Student.h
@@ -1,95 +1,38 @@
-//
-// Created by iamilyasedunov on 26.01.19.
-//
+#ifndef STUDENT_H
+#define STUDENT_H
+
+#include <vector>
+#include <iostream>
 #include <QString>
 #include <string>
 #include <fstream>
-//class Student;
-#include "Group.h"
 
-const int NUM_MARKS = 10;
+class Group;
+class Deanery;
+
+const int NUM_MARKS = 10;           //size for vector marks
 
 class Student {
+                                    //friendly classes
+    friend Deanery;
+    friend Group;
 private:
     int id;
-    std::string fio;
-    //Group &group;
-    int marks[NUM_MARKS];
-    int num = NUM_MARKS;
+    std::string fio;                //first & second name
+    Group* group;                   //link on Group
+    std::vector <int> marks;
+    int num = 0;                    //number of marks
 
 public:
-    Student(int id, std::string fio){
-        this->id = id;              // != 30, 60, 90 там пробелы
-        this->fio = fio;
-        //Group* tmp = new Group();
-
-    }
-    void readMarks(){ //
-        std::ifstream input_file("D:/qt-proj/lab/marks.txt");
-        int numstr = this->id - 1;
-        std::string line;
-        if (input_file.is_open()){
-            while(getline(input_file, line)){
-                if(numstr==0){
-                    //std::cout<<line<<std::endl;
-                    break;
-                }
-                numstr--;
-             }
-        }else{
-            std::cout<<"err";
-        }
-        //const char* strToCh = line.c_str();
-        int index = 0;
-        for(int i = 0;i < NUM_MARKS*2; i++){
-            if(i % 2 == 0)
-                this->marks[index++] = atoi(&line[i]);
-        }
-    }
-
-    void readFio(){
-        std::ifstream input_file("D:/qt-proj/lab/students.txt");
-        int numstr = this->id - 1;
-        std::string line;
-        if (input_file.is_open()){
-            while(getline(input_file, line)){
-                if(numstr==0){
-                    //std::cout<<line<<std::endl;
-                    this->fio = line;
-                    break;
-                }
-                numstr--;
-             }
-        }else{
-            std::cout<<"err";
-        }
-
-    }
-
-    void getInfo(){
-        std::cout<<"id   : "<<this->id<<std::endl;
-        std::cout<<"fio  : "<<this->fio<<std::endl;
-        std::cout<<"marks: ";
-        for(int x: this->marks) std::cout<<x<<" ";
-        std::cout<<std::endl<<"num  : "<<num<<std::endl;
-    }
-
-    void read()
-    {
-        std::string line;
-
-        std::ifstream in("D:/qt-proj/lab/students.txt"); // окрываем файл для чтения
-        if (in.is_open()){
-            while (getline(in, line)){
-                std::cout << line << std::endl;
-            }
-         }
-        else{
-            std::cout << "Err" << std::endl;
-
-        }
-
-        in.close();     // закрываем файл
-     }
-
+    Student(int id, std::string fio);
+    Student(int id);
+    Student(std::string fio);
+    void readMarks();               //marks from file in std::vector <int> marks;
+    void readFio();                 //name & surname in std::string fio;
+    void readId();
+    void getInfo();                 //id, fio, average mark
+    void inGroup(Group *group);     //admission to the group
+    int average();                  //calculation average of marks
 };
+
+#endif
--- src/Group.cpp
+++ src/Group.cpp
@@ -1,23 +1,91 @@
-//
-// Created by iamilyasedunov on 26.01.19.
-//
-#include "../include/Group.h"
-#include "../src/Student.cpp"
-#include "../include/Student.h"
+ #include "Group.h"
 
+Group::Group(std::string title) : title(title){}
 
-const int NUM_ST = 30;
+void Group::addStudent(Student* student){
+    this->students.push_back(student);
+    student->inGroup(this);
+    this->num++;
+}
 
-class Group {
-private:
-    std::string title;
-    Student &students[NUM_ST];
-    int num = NUM_ST;
-    Student &head;
+void Group::getInfo() {
+    std::cout<<"====[GROUP: "<<this->title<<"]===="<<std::endl;
+    std::cout<<"head: "<<this->head->fio<<std::endl;
+    std::cout<<"number of students: "<<this->students.size()<<std::endl;
+    std::cout<<"avearage mark in "<<this->title<<": "<<this->average()<<std::endl;
+    for (auto x: this->students)
+       std::cout<<"id: "<<x->id<<"| fio: "<<x->fio<<"     average mark: "<<x->average()<<std::endl;
+    std::cout<<"====[/GROUP: "<<this->title<<"]===="<<std::endl<<std::endl;
+}
 
-public:
-    Group(Student &head) : head(head){
+void Group::election(){
+    srand(time(nullptr));
+    int index_head = rand() % int(students.size());
+    this->head = students[index_head];
+}
+
+Student* Group::search(std::string fio, int id){
+    for(auto x: this->students){
+        if ((x->fio == fio)&&(x->id == id)) {
+            return x;
+        }
+    }
+    return nullptr;
+}
+
+Student* Group::search(std::string fio){
+    for(auto x: this->students){
+        if (x->fio == fio) {
+            return x;
+        }
+    }
+    return nullptr;
+}
+
+Student* Group::search(int id){
+    for(auto x: this->students){
+        if (x->id == id) {
+            return x;
+        }
+    }
+    return nullptr;
+}
+
+int Group::searchTwoness(){
+    for(auto x: this->students){
+        if (x->average() < PASS_GRADE) {
+            return x->id;
+        }
+    }
+    return -1;
+}
+
+int Group::average(){
+    int sum = 0;
+    for (auto x: this->students)
+        sum+=x->average();
+    return sum/int(this->students.size());
+}
+
+bool Group::expell(int id){
+    Student* student = this->search(id);
+    if (this->head == student){
+        this->election();
     }
 
+    if (student == nullptr)
+        return false;
+    auto pos = find(this->students.begin(), this->students.end(), student) - this->students.begin();
+    this->students.erase(this->students.begin() + pos);
+    return true;
+}
 
-};
+bool Group::expell(std::string fio){
+    Student* student = this->search(fio);
+    if (student == nullptr)
+        return false;
+    auto pos = find(students.begin(), students.end(), student) - students.begin();
+    students.erase(students.begin() + pos);
+    return true;
+}
+
